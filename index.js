fb1fde9edd4ec682efc945cc139bf6ab1bfff6f8
Nikhil Sampangi <nikhil.sampangi@gmail.com>
2019-06-18 16:23:32 +0530

    projects nd contact

--- index.js
+++ index.js
@@ -30,3 +30,25 @@ $(document).ready(function() {
     $(".be").addClass("btn-outline-primary");
   });
 });
+
+$(document).ready(function() {
+  $(".block-5").mouseover(function() {
+    $(".bp").removeClass("btn-outline-primary");
+    $(".bp").addClass("btn-primary animated infinite pulse");
+  });
+  $(".block-5").mouseout(function() {
+    $(".bp").removeClass("btn-primary animated infinite pulse");
+    $(".bp").addClass("btn-outline-primary");
+  });
+});
+
+$(document).ready(function() {
+  $(".block-6").mouseover(function() {
+    $(".bc").removeClass("btn-outline-primary");
+    $(".bc").addClass("btn-primary animated infinite pulse");
+  });
+  $(".block-6").mouseout(function() {
+    $(".bc").removeClass("btn-primary animated infinite pulse");
+    $(".bc").addClass("btn-outline-primary");
+  });
+});
