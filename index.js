08d0f0518a5dbc1dbae31eb3084f947c06c31361
Nikhil Sampangi <nikhil.sampangi@gmail.com>
2019-06-18 11:00:22 +0530

    skills nd experience

--- index.js
+++ index.js
@@ -8,3 +8,25 @@ $(document).ready(function() {
     $(".ba").addClass("btn-outline-primary");
   });
 });
+
+$(document).ready(function() {
+  $(".block-3").mouseover(function() {
+    $(".bs").removeClass("btn-outline-primary");
+    $(".bs").addClass("btn-primary animated infinite pulse");
+  });
+  $(".block-3").mouseout(function() {
+    $(".bs").removeClass("btn-primary animated infinite pulse");
+    $(".bs").addClass("btn-outline-primary");
+  });
+});
+
+$(document).ready(function() {
+  $(".block-4").mouseover(function() {
+    $(".be").removeClass("btn-outline-primary");
+    $(".be").addClass("btn-primary animated infinite pulse");
+  });
+  $(".block-4").mouseout(function() {
+    $(".be").removeClass("btn-primary animated infinite pulse");
+    $(".be").addClass("btn-outline-primary");
+  });
+});
