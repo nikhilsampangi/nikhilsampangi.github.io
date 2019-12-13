c53b89d684c0122ff1143d2c4fdc4394770fbf48
nikhilsampangi <nikhil.sampangi@gmail.com>
2019-12-13 16:04:41 +0530

    ver 2

--- index.js
+++ index.js
@@ -1,54 +1,62 @@
-$(document).ready(function() {
-  $(".block-2").mouseover(function() {
+$(document).ready(function () {
+  $(".block-2").mouseover(function () {
     $(".ba").removeClass("btn-outline-primary");
     $(".ba").addClass("btn-primary animated infinite pulse");
   });
-  $(".block-2").mouseout(function() {
+  $(".block-2").mouseout(function () {
     $(".ba").removeClass("btn-primary animated infinite pulse");
     $(".ba").addClass("btn-outline-primary");
   });
 });
 
-$(document).ready(function() {
-  $(".block-3").mouseover(function() {
+$(document).ready(function () {
+  $(".block-3").mouseover(function () {
     $(".bs").removeClass("btn-outline-primary");
     $(".bs").addClass("btn-primary animated infinite pulse");
   });
-  $(".block-3").mouseout(function() {
+  $(".block-3").mouseout(function () {
     $(".bs").removeClass("btn-primary animated infinite pulse");
     $(".bs").addClass("btn-outline-primary");
   });
 });
 
-$(document).ready(function() {
-  $(".block-4").mouseover(function() {
+$(document).ready(function () {
+  $(".block-4").mouseover(function () {
     $(".be").removeClass("btn-outline-primary");
     $(".be").addClass("btn-primary animated infinite pulse");
   });
-  $(".block-4").mouseout(function() {
+  $(".block-4").mouseout(function () {
     $(".be").removeClass("btn-primary animated infinite pulse");
     $(".be").addClass("btn-outline-primary");
   });
 });
 
-$(document).ready(function() {
-  $(".block-5").mouseover(function() {
+$(document).ready(function () {
+  $(".block-5").mouseover(function () {
     $(".bp").removeClass("btn-outline-primary");
     $(".bp").addClass("btn-primary animated infinite pulse");
   });
-  $(".block-5").mouseout(function() {
+  $(".block-5").mouseout(function () {
     $(".bp").removeClass("btn-primary animated infinite pulse");
     $(".bp").addClass("btn-outline-primary");
   });
 });
 
-$(document).ready(function() {
-  $(".block-6").mouseover(function() {
+$(document).ready(function () {
+  $(".block-6").mouseover(function () {
     $(".bc").removeClass("btn-outline-primary");
     $(".bc").addClass("btn-primary animated infinite pulse");
   });
-  $(".block-6").mouseout(function() {
+  $(".block-6").mouseout(function () {
     $(".bc").removeClass("btn-primary animated infinite pulse");
     $(".bc").addClass("btn-outline-primary");
   });
+});
+
+// Auto Collapse navbar on select in small screens
+
+$('.nav-link').on('click', function () {
+  if (screen.width <= 768) {
+    $('.navbar-toggler').click(); //bootstrap 4.x
+  }
 });
